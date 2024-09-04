c3073bb16a1e6cb8edf684cb4c5ddfb6bd44d941
Aderlangio123 <faderlangio123@gmail.com>
2024-09-04 15:30:20 -0300

    atividade 05 - minuto em seg

---
+++ atividade05.py
@@ -0,0 +1,7 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+
+valorMinuto = int(input("digite a quantidade de minutos que deseja converter!  "))
+
+conversao = valorMinuto * 60
+
+print("o valor em minutos é ", conversao)
